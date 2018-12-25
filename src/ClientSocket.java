b11c8e8fb03d25347d0c449d54b7a8549221999d
todd <kj4ntv@gmail.com>
2018-12-25 05:04:01 -0800

    Add some basic code

--- src/ClientSocket.java
+++ src/ClientSocket.java
@@ -1,2 +1,15 @@
-public class ClientSocket {
+import java.net.Socket;
+import java.net.UnknownHostException;
+import java.io.IOException;
+
+public class ClientSocket extends Socket {
+
+    public ClientSocket (String url) throws UnknownHostException, IOException {
+
+        super(url, 80);
+
+    }
+
+
+
 }

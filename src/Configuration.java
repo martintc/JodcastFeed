69fd49ac9ec79c76ed7babc34b316f5dda186086
todd <kj4ntv@gmail.com>
2018-12-30 02:43:58 -0800

    Base for loading a future configuration file

--- src/Configuration.java
+++ src/Configuration.java
@@ -1,6 +1,11 @@
+import java.io.File;
+import java.util.Scanner;
+import java.io.FileNotFoundException;
+
 public class Configuration {
 
     private String saveLocation;
+
 
     public Configuration () {
 
@@ -13,6 +18,22 @@ public class Configuration {
 
     public String getSaveLocation () {
         return saveLocation;
+    }
+
+    public boolean loadConfiguration (String filePath) {
+        try {
+            Scanner in = new Scanner(new File(filePath));
+            while (in.hasNextLine()) {
+                saveLocation = in.nextLine();
+            }
+            return true;
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+            return false;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return false;
+        }
     }
 
 }

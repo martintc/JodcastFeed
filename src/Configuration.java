c6813da737370ca7b366177b5b3d9605d0d9a39f
todd <kj4ntv@gmail.com>
2018-12-30 02:50:35 -0800

    Write to config file and podcastfilepath

--- src/Configuration.java
+++ src/Configuration.java
@@ -1,10 +1,13 @@
 import java.io.File;
 import java.util.Scanner;
 import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.io.PrintWriter;
 
 public class Configuration {
 
     private String saveLocation;
+    private String podcastFilePath;
 
 
     public Configuration () {
@@ -16,6 +19,15 @@ public class Configuration {
         return true;
     }
 
+    public boolean loadPodcastFilePath (String pString) {
+        podcastFilePath = pString;
+    }
+
+    public String getPodcastFilePath () {
+        return podcastFilePath;
+    }
+
+
     public String getSaveLocation () {
         return saveLocation;
     }
@@ -25,6 +37,23 @@ public class Configuration {
             Scanner in = new Scanner(new File(filePath));
             while (in.hasNextLine()) {
                 saveLocation = in.nextLine();
+                podcastFilePath = in.nextLine();
+            }
+            return true;
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+            return false;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+    public boolean writeConfiguration (String filePath, ArrayList<String> settings) {
+        try {
+            PrintWriter out = new PrintWriter(new File(filePath));
+            for (String s : settings) {
+                out.println(s);
             }
             return true;
         } catch (FileNotFoundException e) {
